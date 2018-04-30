24ab301e1f0f9a05e4801334f506aa062ce7b1c9
Elelion <Elelion@yandex.ru>
2018-04-30 16:23:57 +0400

    Quest-2: выполнил задание

--- index.php
+++ index.php
@@ -1,9 +1,61 @@
 <?php
-$is_auth = (bool) rand(0, 1);
+//QUEST_1:
 
-$user_name = 'Константин';
-$user_avatar = 'img/user.jpg';
+    //$is_auth - Переменная определяющая зарегистрировался ли(true) пользователь или нет(false)
+    //Присваиваем рандомное значение 1 или 0
+    $is_auth = (bool) rand(0, 1);
+
+    $user_name = 'Константин';
+
+    //Присваиваем картинку переменной
+    $user_avatar = "img/user.jpg";
+
+//QUEST_2.1: Добавьте в код файла index.php простой массив категорий со следующими значениями:
+    //простой массив категорий 
+    $set_product = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+
+//QUEST_2.2: Добавьте двумерный массив, каждый элемент которого будет ассоциативным массивом с данными одного объявления. 
+    //ассоциативный массив. Где FullProduct = далее как FP
+    $full_product = [
+        [
+        'FP_name' => '2014 Rossignol District Snowboard',
+        'FP_category' => 'Доски и лыжи',
+        'FP_price' => '10999',
+        'FP_url' => 'img/lot-1.jpg'
+        ],
+        [
+        'FP_name' => 'DC Ply Mens 2016/2017 Snowboard',
+        'FP_category' => 'Доски и лыжи',
+        'FP_price' => '159999',
+        'FP_url' => 'img/lot-2.jpg'
+        ],
+        [
+        'FP_name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'FP_category' => 'Крепления',
+        'FP_price' => '8000',
+        'FP_url' => 'img/lot-3.jpg'
+        ],
+        [
+        'FP_name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'FP_category' => 'Ботинки',
+        'FP_price' => '10999',
+        'FP_url' => 'img/lot-4.jpg'
+        ],
+        [
+        'FP_name' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'FP_category' => 'Одежда',
+        'FP_price' => '7500',
+        'FP_url' => 'img/lot-5.jpg'
+        ],
+        [
+        'FP_name' => 'Маска Oakley Canopy',
+        'FP_category' => 'Разное',
+        'FP_price' => '5400',
+        'FP_url' => 'img/lot-6.jpg'
+        ],
+    ];
 ?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -28,7 +80,31 @@ $user_avatar = 'img/user.jpg';
 
         <nav class="user-menu">
 
-        <!-- здесь должен быть PHP код для показа аватара пользователя -->
+        <!-- QUEST-1: Условие должно проверять истинность значения переменной $is_auth (сравнивать с истиной) -->
+        <?php
+        if ($is_auth == true) {
+            ?>
+                <div class="user-menu__image">
+                    <img src="<?php echo $user_avatar ?>" width="40" height="40" alt="Пользователь">
+                </div>
+                <div class="user-menu__logged">
+                    <p><?php echo $user_name ?></p>
+                </div>
+            <?php
+        	} else {
+            ?>
+                <ul class="user-menu__list">
+                    <li class="user-menu__item">
+                    <a href="#">Регистрация</a>
+                </li>
+
+                <li class="user-menu__item">
+                    <a href="#">Вход</a>
+                </li>
+                </ul>
+            <?php
+        	}
+        ?>
 
         </nav>
     </div>
@@ -64,17 +140,22 @@ $user_avatar = 'img/user.jpg';
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
+
+<!-- QUEST_2.4: Объявления выводятся как элементы списка ul.lots__list. Используя цикл для прохода по массиву с лотами, 
+								заполните этот список объявлениями. Каждый элемент списка (объявление) должно подставлять информацию в теги 
+								из соответствующего ключа массива. -->
+        	<?php foreach($full_product as $FP) { ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                    <img src="<?= $FP['FP_url']; ?>" width="350" height="260" alt="Сноуборд">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
+                    <span class="lot__category"><?= $FP['FP_category']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $FP['FP_name']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+                            <span class="lot__cost"><?= $FP['FP_price']; ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
 
@@ -82,6 +163,7 @@ $user_avatar = 'img/user.jpg';
                     </div>
                 </div>
             </li>
+          <?php } ?>
         </ul>
     </section>
 </main>
@@ -89,9 +171,17 @@ $user_avatar = 'img/user.jpg';
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
+
+<!-- QUEST_2.3: Найдите в HTML-коде список ul.nav__list. Используйте цикл, чтобы заменить содержимое списка данными из массива категорий./li> -->
+        	<?php foreach($set_product as $SP) { ?>
             <li class="nav__item">
-                <a href="all-lots.html">Доски и лыжи</a>
+                <a href="all-lots.html"><?php print($SP); ?></a>
             </li>
+          <?php } ?>
+<!--        
+						<li class="nav__item">
+                <a href="all-lots.html">Доски и лыжи</a>
+            </li>    
             <li class="nav__item">
                 <a href="all-lots.html">Крепления</a>
             </li>
@@ -106,7 +196,8 @@ $user_avatar = 'img/user.jpg';
             </li>
             <li class="nav__item">
                 <a href="all-lots.html">Разное</a>
-            </li>
+            </li>   
+-->           
         </ul>
     </nav>
     <div class="main-footer__bottom container">
