514ff3bf5f56f0c8a8f8fa60f633d9a5e67acb14
Elelion <Elelion@yandex.ru>
2018-05-10 17:10:18 +0400

    Поправил schema.sql

--- schema.sql
+++ schema.sql
@@ -1,27 +1,35 @@
-﻿CREATE TABLE category (
+﻿CREATE DATABASE YetiCave CHARACTER SET 'utf8';
+
+
+CREATE TABLE category (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(64)
 );
-CREATE INDEX index_name ON category(name);
 
 CREATE TABLE lot (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	date_create DATETIME,
-	title VARCHAR(64),
+	title VARCHAR(255),
 	description VARCHAR(255),
 	image_file VARCHAR(255),
 	start_price INT(8),
 	date_completed DATETIME,
-	step_price INT(8)
-);
-CREATE INDEX index_title ON lot(title);
+	step_price INT(8),
+	
+	author_id INT(8),
+	winner_id INT(8),
+	category_id INT(8)	
+) 
+ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 CREATE TABLE rate (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	date_create DATETIME,
-	amount INT(8)
+	amount INT(8),
+
+	user_id INT(8),
+	lot_id INT(8)
 );
-CREATE INDEX index_amount ON rate(amount);
 
 CREATE TABLE user (
 	id INT AUTO_INCREMENT PRIMARY KEY,
@@ -30,7 +38,23 @@ CREATE TABLE user (
 	username VARCHAR(32),
 	password CHAR(64),
 	avatar VARCHAR(255),
-	contact VARCHAR(64)
+	contact VARCHAR(64),
+
+	lot_id INT(8),
+	rate_id INT(8)
 );
-CREATE INDEX index_name ON user(name);
+
+
+CREATE INDEX index_title ON lot(title);
+CREATE INDEX index_desc ON lot(description);
+CREATE FULLTEXT INDEX index_description ON lot(description);
+ALTER TABLE lot ADD FOREIGN KEY (author_id) REFERENCES user(id);
+ALTER TABLE lot ADD FOREIGN KEY (winner_id) REFERENCES user(id);
+ALTER TABLE lot ADD FOREIGN KEY (category_id) REFERENCES category(id);
+
+ALTER TABLE rate ADD FOREIGN KEY (user_id) REFERENCES user(id);
+ALTER TABLE rate ADD FOREIGN KEY (lot_id) REFERENCES lot(id);
+
 CREATE UNIQUE INDEX index_email ON user(email);
+ALTER TABLE user ADD FOREIGN KEY (lot_id) REFERENCES lot(id);
+ALTER TABLE user ADD FOREIGN KEY (rate_id) REFERENCES rate(id);
