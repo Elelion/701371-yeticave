3c289a8c5f3d534985bb6b41d285db065b149152
Elelion <Elelion@yandex.ru>
2018-05-17 23:49:27 +0400

    QUEST_5: changed a sql request and this outputing

--- index.php
+++ index.php
@@ -1,31 +1,19 @@
 <?php
+	require_once 'src/functions.php';
+
     date_default_timezone_set('Europe/Moscow');
 
 //QUEST_5.1: В сценарии главной страницы выполните подключение к MySQL
-    $link = mysqli_connect("127.0.0.1", "root", "", "yeticave");
-    mysqli_set_charset($link, "utf8");
+    //$link - присваиваем ему ф-цию.
 
-    if (!$link) {
-        echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
-        echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
-        echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
-        exit;
-    }
+    $link = connectDB();    
 
 //QUEST_5.4: Отправьте SQL-запрос для получения списка категорий
     $sql = "SELECT * FROM category";
     $result = mysqli_query($link, $sql); //Подключаемся к БД и получаем результат (в переменную) от нашего вышеуказанного запроса SQL        
-    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC); //MYSQLI_ASSOC - константа, которая возвращает массив в читабельном виде, (т.е. в виде матрици на вид как бы)
-
-    //Выводим наши данные из БД в массив category_list[], выводим его ниже в QUEST_5.5...
-    foreach ($rows as $row) {
-        $category_list[] = $row['name'];            
-    }
-
-    require_once 'src/functions.php';
+    $category_list = mysqli_fetch_all($result, MYSQLI_ASSOC); //MYSQLI_ASSOC - константа, которая возвращает массив в читабельном виде, (т.е. в виде матрици на вид как бы)
 
 //QUEST_5.2: Отправьте SQL-запрос для получения всей информации по новым лотам
-
     //Выводим наши данные из БД в массив category_list[], выводим его ниже...    
     $sql_lot = "SELECT 
                 title AS name, 
@@ -35,10 +23,9 @@
                 FROM lot 
                 INNER JOIN category ON lot.category_id = category.id";
     
-    $result_lot = mysqli_query($link, $sql_lot);
-    
+    $result_lot = mysqli_query($link, $sql_lot);    
     $product_list = mysqli_fetch_all($result_lot, MYSQLI_ASSOC);
-    
+
     $is_auth = (bool) rand(0, 1);
     $user_name = 'Константин';
     $user_avatar = "img/user.jpg";
@@ -51,6 +38,5 @@
 //QUEST_5.5: Используйте эти данные для показа списка категорий в футере страницы (вывод остается преждним как и ранее, лишь беруться данные из БД)
     print(render('layout', ['title' => $title, 'content' => $content, 'user_name' => $user_name, 'user_avatar' => $user_avatar, 'is_auth' => $is_auth,
         'category_list' => $category_list]));
-
 
 //НЕ ЗАКРЫВАТЬ PHP!!!...
